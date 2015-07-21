19f19be4eef9c42ae14bf9251d02485863e833ef
Martin Cerny <martin.cerny@northumbria.ac.uk>
2015-07-21 09:13:20 +0200

    Service doesn't work. Need to discuss with Jan Skurla.

--- BimLibraryAddin/AddIns/ProductSearchAddIn.cs
+++ BimLibraryAddin/AddIns/ProductSearchAddIn.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 using Autodesk.Revit.UI;
@@ -11,6 +12,20 @@ namespace BimLibraryAddin.AddIns
     {
         public Result Execute(ExternalCommandData commandData, ref string message, Autodesk.Revit.DB.ElementSet elements)
         {
+            
+
+            //show UI
+
+            //get search string
+
+            //search for a Revit products
+            var proxy = new BimLibraryService.BIMserviceClient(new BasicHttpBinding(BasicHttpSecurityMode.Message),
+                new EndpointAddress("http://www.narodni-bim-knihovna.cz/BIMservice.svc"));
+            proxy.Open();
+            var products = proxy.GetProductByName("test");
+
+            //show search results with ability to import data into Revit
+
             throw new NotImplementedException();
         }
     }
---
+++ BimLibraryServiceTests/ProductRetrievalTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ServiceModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BimLibraryServiceTests
+{
+    [TestClass]
+    public class ProductRetrievalTests
+    {
+        [TestMethod]
+        public void EstablishingConnection()
+        {
+            var proxy = new BimLibraryService.BIMserviceClient(new BasicHttpBinding(BasicHttpSecurityMode.None),
+                new EndpointAddress("http://www.narodni-bim-knihovna.cz/BIMservice.svc"));
+            proxy.Open();
+        }
+
+        [TestMethod]
+        public void ProductSearch()
+        {
+            var client = new BimLibraryService.BIMserviceClient(new BasicHttpBinding(BasicHttpSecurityMode.None),
+                new EndpointAddress("http://www.narodni-bim-knihovna.cz/BIMservice.svc"));
+            client.Open();
+            var products = client.GetProductByName("test");
+
+            foreach (var product in products)
+            {
+                var description = product.FullDescription;
+                var name = product.Name;
+                var isPublished = product.Published;
+                var usDeleted = product.Deleted;
+                var downloadId = product.DownloadId;
+            }
+
+        }
+    }
+}
